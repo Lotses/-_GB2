25342cb65a2aaf535205352c4eec55284616bda3
Lotses <Lotses@mail.ru>
2024-01-30 20:46:59 +0300

    les 1

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,3 +1,25 @@
-﻿System.Console.WriteLine("Введите первое целое число");
-int first_int = Console.ReadLine();
-System.Console.WriteLine("Введите второе целое число");
+﻿// System.Console.WriteLine("Введите первое целое число");
+// int first_int = Convert.ToInt32( Console.ReadLine() );
+// System.Console.WriteLine("Введите второе целое число");
+// int second_int = Convert.ToInt32( Console.ReadLine() );
+
+// if (first_int == second_int * second_int)
+// {
+//     System.Console.WriteLine("да");
+// }
+// else
+//     System.Console.WriteLine("нет");
+
+
+using System.Globalization;
+
+System.Console.WriteLine("Введите целое число");
+int f_int = Convert.ToInt32(Console.ReadLine());
+
+int i = - f_int;
+while (i < f_int)
+{
+    System.Console.Write(i + ",");
+    i += 1;
+}
+System.Console.WriteLine(i);
